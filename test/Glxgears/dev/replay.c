fba522dfce6465ceb002c060ddae65d0b97e6303
Erwin <34348385+ErwinRussel@users.noreply.github.com>
2022-06-12 10:53:07 -0700

    Update replay.c

--- test/Glxgears/dev/replay.c
+++ test/Glxgears/dev/replay.c
@@ -3,7 +3,24 @@
 #include <string.h>
 #include <stdlib.h>
 #include "atl_lh.h"
-#include "replay.h"
+//#include "replay.h"
+
+Display* cur_display_p;
+Window cur_window;
+Window cur_root_window;
+XVisualInfo* cur_vis_info_p;
+int cur_default_screen;
+Colormap cur_colormap;
+GLXContext cur_context;
+XSizeHints cur_hints;
+
+// StrgBlock
+struct StrgBlock {
+    message_types message_type;
+    data_types data_type;
+    int payload_size;
+    char buffer[SHMEM_BUFF];
+};
 
 FILE *file;
 
