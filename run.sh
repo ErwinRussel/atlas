7614f419f19272f2a75341177cf561c69991e0d8
Erwin <34348385+ErwinRussel@users.noreply.github.com>
2022-06-12 15:07:25 -0700

    update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+
 
 # todo: check if restore env variable is there
 # todo: see where the file is
@@ -7,17 +7,19 @@
 #echo $ATL_RESTORE
 #if [ "$ATL_RESTORE" == "TRUE" ]
 #then
-#  echo "Restoring atlas"
-#  nohup test/Glxgears/dev/atl_lh restore calllog.bin &
-#  sleep 1
-#  criu restore
+echo "Restoring atlas"
+nohup test/Glxgears/dev/atl_lh replay calllog.bin &
+echo "$!" > lh_pid
+sleep 5
+criu restore -v4 --images-dir ./checkpoints --shell-job &
+echo "$!" > uh_pid
 ## NOT RESTORE
 #else
-echo "Running atlas"
-nohup strangle 30 test/Glxgears/dev/atl_lh &
-echo "$!" > lh_pid
-sleep 1
-LD_PRELOAD=$(pwd)/test/Glxgears/dev/atl_lib.so test/Glxgears/dev/glxgears &
-echo "$!" > uh_pid
+#echo "Running atlas"
+#nohup strangle 30 test/Glxgears/dev/atl_lh &
+#echo "$!" > lh_pid
+#sleep 0.5
+#LD_PRELOAD=$(pwd)/test/Glxgears/dev/atl_lib.so test/Glxgears/dev/glxgears &
+#echo "$!" > uh_pid
 
 
