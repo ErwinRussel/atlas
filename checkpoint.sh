447c61e1fdc02c9da0b37b35888353ff5ea84a34
Erwin <34348385+ErwinRussel@users.noreply.github.com>
2022-06-12 14:17:38 -0700

    update checkpint and run

--- checkpoint.sh
+++ checkpoint.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
+
 # send the glxgears process a SIGUSER1
-kill -SIGUSR1 $UH_PD
+UH_PID=`cat uh_pid`
+LH_PID=`cat lh_pid`
+kill -10 "$UH_PID"
+kill "$LH_PID"
--- run.sh
+++ run.sh
@@ -4,19 +4,20 @@
 # todo: see where the file is
 
 # RESTORE
-echo $ATL_RESTORE
-if [ "$ATL_RESTORE" == "TRUE" ]
-then
-  echo "Restoring atlas"
-  nohup test/Glxgears/dev/atl_lh restore calllog.bin &
-  sleep 1
-  criu restore
-# NOT RESTORE
-else
-  echo "Running atlas"
-  nohup strangle 30 test/Glxgears/dev/atl_lh &
-  sleep 1
-  LD_PRELOAD=$(pwd)/test/Glxgears/dev/atl_lib.so test/Glxgears/dev/glxgears
-fi
+#echo $ATL_RESTORE
+#if [ "$ATL_RESTORE" == "TRUE" ]
+#then
+#  echo "Restoring atlas"
+#  nohup test/Glxgears/dev/atl_lh restore calllog.bin &
+#  sleep 1
+#  criu restore
+## NOT RESTORE
+#else
+echo "Running atlas"
+nohup strangle 30 test/Glxgears/dev/atl_lh &
+echo "$!" > lh_pid
+sleep 1
+LD_PRELOAD=$(pwd)/test/Glxgears/dev/atl_lib.so test/Glxgears/dev/glxgears &
+echo "$!" > uh_pid
 
-trap "echo hello" SIGINT
+
