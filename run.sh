06007464ce0a0e4250feab064f19925a290e7ab9
Erwin <34348385+ErwinRussel@users.noreply.github.com>
2022-06-28 00:52:49 -0700

    take some time

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ then
   nohup strangle 30 test/Glxgears/dev/atl_lh replay checkpoints/calllog.bin >/dev/null 2>&1 &
 #  test/Glxgears/dev/atl_lh replay calllog.bin &
   echo "$!" > lh_pid
-  sleep 0.3
+  sleep 1
   ps aux
   criu restore --images-dir ./checkpoints --shell-job -d
 # PID should restore the same
@@ -17,7 +17,7 @@ else
   nohup strangle 30 test/Glxgears/dev/atl_lh >/dev/null 2>&1 &
 #  strangle 30 test/Glxgears/dev/atl_lh &
   echo "$!" > lh_pid
-  sleep 0.3
+  sleep 1
   LD_PRELOAD=$(pwd)/test/Glxgears/dev/atl_lib.so test/Glxgears/dev/glxgears &
   echo "$!" > uh_pid
   ps aux
